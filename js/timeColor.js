54225c6aae1f4bfd35a3c93b0cb5c60cdb9024dd
andela-ladenusi <ladi.adenusi@andela.co>
2014-12-17 16:07:54 +0100

    TimeOColor refactoring with Abimbola Idowu

--- js/timeColor.js
+++ js/timeColor.js
@@ -1,37 +1,41 @@
 var appTimeColor = {
+
   init: function() {
     return this.clock();
   },
+  
   clock: function() {
     var self = this;
-    var now = new Date(),
-        h = now.getHours(),
-        m = now.getMinutes(),
-        s = now.getSeconds();
+    var now = new Date();
 
-    m = self.checkTime(m);
-    s = self.checkTime(s);
+    h = self.checkTime(now.getHours());
+    m = self.checkTime(now.getMinutes());
+    s = self.checkTime(now.getSeconds());
 
     var t = setTimeout(function(){
-      self.clock()
+      self.clock();
     }, 1000);
-    document.getElementById('time').innerHTML = h + ' : ' + m + ' : ' + s; 
+    document.getElementById("time").innerHTML = h + ' : ' + m + ' : ' + s; 
     return self.makeHexCode(h, m, s);
   },
+
   checkTime: function(i) {
     if(i < 10) {
       i = "0" + i;
     }
     return i;
   },
+
   makeHexCode: function(h, m, s) {
     var hexcode = '#' + h + m + s;
+    // console.log(hexcode);
     document.body.bgColor = hexcode;
     document.getElementById('hexCode').innerHTML = hexcode;
   }
 };
 
-window.onload = appTimeColor.init();
+
+(function(){appTimeColor.init()})();
 
 
 
