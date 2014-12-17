657a1c9c0bf89505ee90f65a1b3729120cb49837
Abimbola Idowu <abimbola2010@gmail.com>
2014-12-17 18:21:27 +0100

    Added a blinking cursor to the app

--- js/timeColor.js
+++ js/timeColor.js
@@ -1,4 +1,13 @@
 var appTimeColor = {
+
+  timeContainer : document.getElementById('time'),
+  hour : document.getElementById('hour'),
+  min : document.getElementById('min'),
+  sec : document.getElementById('sec'),
+  colon1 : document.getElementById('colon1'),
+  colon2 : document.getElementById('colon2'),
+  showColon: true,
+
   init: function() {
     return this.clock();
   },
@@ -15,8 +24,18 @@ var appTimeColor = {
     var t = setTimeout(function(){
       self.clock()
     }, 1000);
-    document.getElementById('time').innerHTML = h + ' : ' + m + ' : ' + s; 
+    this.updateTime(h,m,s);
+    // this.createColon() + this.createTimeElement(m) + this.createColon() + this.createTimeElement(s); 
     return self.makeHexCode(h, m, s);
+  },
+
+  updateTime: function (h,m,s) {
+    this.hour.innerHTML = h;
+    this.colon1.innerHTML = ':';
+    this.min.innerHTML = m;
+    this.colon2.innerHTML = ':';
+    this.sec.innerHTML = s;
+    this.blinkingColon();
   },
   checkTime: function(i) {
     if(i < 10) {
@@ -28,6 +47,17 @@ var appTimeColor = {
     var hexcode = '#' + h + m + s;
     document.body.bgColor = hexcode;
     document.getElementById('hexCode').innerHTML = hexcode;
+  },
+
+  blinkingColon: function () {
+    this.showColon = !this.showColon;
+    if (this.showColon) {
+    colon1.setAttribute('style', 'visibility:hidden');
+    colon2.setAttribute('style', 'visibility:hidden');
+    } else {
+    colon1.setAttribute('style', 'visibility:visible');
+    colon2.setAttribute('style', 'visibility:visible');
+    }
   }
 };
 
